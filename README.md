0347ee7f6d81616412773bf5a008f72fa40dca44
franck <franck.kone@mykenzie.snhu.edu>
2023-04-25 10:48:42 -0400

    completed jsdom roll dice assessment including optional pratice week 14

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 Complete your work inside of `code.js`
 
 Follow the instructions provided on my.kenzie.academy for this assignment.
+
+
+To make the barGraph div be the first child of the graph div:
+
+souuce : https://developer.mozilla.org/en-US/docs/Web/API/Node/insertBefore
+
+so i changed the text content of the graph div, and put it inside a span element to make it be a child, and then inserted the second child (the bargraph div), and finally switched their position using that method  
--- code.js
+++ code.js
@@ -1,10 +1,12 @@
 let results = [null, null, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 // Your Code Here!
+let button = document.getElementById('roll'), resultDiv = document.getElementById('result')
+let graph = document.getElementById('graph');
+let btnThousandRoll = document.getElementById('rollThousandTimes');
 
-// Create a function called rollDie() which returns a random number between 1 - 6. Then make another function which rolls two dice and adds together the sum.
-
-// 💡 Hint: The second function can call your * rollDie() * function! If you call it twice, you can then add the two numbers together to get the sum.
+button.addEventListener('click', pairDice);
+btnThousandRoll.addEventListener('click', Roll1000Function);
 
 function rollDie() {
 
@@ -14,30 +16,12 @@ function rollDie() {
 function sumTwoRolledDice(dice1, dice2) {
 
     return dice1 + dice2
-
 }
-// sumTwoRolledDice(rollDie(), rollDie())
-
-// In the HTML, there is already a button with the id roll and a div with the id result.Using JavaScript, get both of those items by id and assign them to variables.
-
-let button = document.getElementById('roll'), resultDiv = document.getElementById('result')
-
-// Create an event listener on the button for the "click" event.Make a callback function and pass it into the click event.
-
-// In this callback function, call your rollDie() method and store the number in a variable.
-
-// Output a sentence stating what number was rolled to the "result" div.
-
-// Your app should now be working like the app above!
-let graph = document.getElementById('graph')
-
-button.addEventListener('click', pairDice)
 
 function pairDice() {
     let diceResult = sumTwoRolledDice(rollDie(), rollDie())
     results[diceResult] += 1
 
-    //to remove all children of graph node before displaying the updated graph
     while (graph.firstChild) {
         graph.removeChild(graph.firstChild)
     }
@@ -45,24 +29,18 @@ function pairDice() {
 
     resultDiv.textContent = `You Rolled a ${diceResult}!`
 
-    changeBarGraphHeight()
-
+    barGraphHeightPairDice()
 }
 
-// Then render each value of the results array inside of the "graph" div.
-
-// To do this, create a new function renderGraph().Have that function loop through the results array.For every value create a div and add it to the graph, adding the number into the div.
 function renderGraph() {
     for (let result = 2; result < results.length; result++) {
-        let newResultDiv = document.createElement('div')
 
+        let newResultDiv = document.createElement('div')
         let span = document.createElement('span')
 
         span.textContent = `You rolled a ${result} : ${results[result]} times`
         newResultDiv.append(span)
-        // newResultDiv.textContent = `You rolled a ${result} : ${results[result]} times`
 
-        //style newResultDiv
         newResultDiv.style.marginLeft = '10px'
         newResultDiv.style.padding = '10px'
         newResultDiv.style.background = 'lightgray'
@@ -70,68 +48,43 @@ function renderGraph() {
         newResultDiv.style.height = '300px'
         newResultDiv.style.fontSize = '15px'
 
-
         graph.append(newResultDiv)
-        // style graph
         graph.style.marginTop = '20px'
         graph.style.display = 'flex'
     }
 }
-
-// Next, add another button to your HTML and give it an id.This button should Roll 1000 pairs of dice!
-
-// Create the event listener and callback just like the other button.Have your callback run a loop 1000 times, rolling a pair of dice and recording the result into the results array each time.
-
-let btnThousandRoll = document.getElementById('rollThousandTimes')
-btnThousandRoll.addEventListener('click', Roll1000Function)
 
 function Roll1000Function() {
     for (let roll = 1; roll <= 1000; roll++) {
         pairDice()
     }
     resultDiv.textContent = 'You Rolled 1000 times!'
-    changeBarGraphHeight()
-
 }
 
-function changeBarGraphHeight() {
-    let totalHeight = 0
+function barGraphHeightPairDice() {
+
     let resultMax = Math.max(...results)
-    console.log(resultMax)
-    for (let result = 2; result < results.length; result++) {
-        totalHeight += results[result]
 
-
-    }
     for (let child = 0; child < graph.children.length; child++) {
-        // graph.children[child].style.height = ((results[child + 2]/ totalHeight)*100) + "px"
-        console.log(results[child])
-        if (results[child] === resultMax) {
+     
+        if (results[child +2] === resultMax) {
             let barGraph = document.createElement('div')
             barGraph.style.width = '100%'
             barGraph.style.height = "90%"
-            barGraph.style.background = "blue"
+            barGraph.style.background = "red"
 
             graph.children[child].append(barGraph)
             graph.children[child].insertBefore(barGraph, graph.children[child].firstChild)
 
-        } else if (results[child] < resultMax){
+        } else if (results[child +2] < resultMax){
 
             let barGraph = document.createElement('div')
             barGraph.style.width = '100%'
             barGraph.style.height = ((results[child + 2] / resultMax) * 90) + "%"
-            barGraph.style.background = "blue"
+            barGraph.style.background = "orange";
 
-            graph.children[child].append(barGraph)
-            graph.children[child].insertBefore(barGraph, graph.children[child].firstChild)
-
+            graph.children[child].append(barGraph);
+            graph.children[child].insertBefore(barGraph, graph.children[child].firstChild);
         }
-
-
     }
 }
-
-
-//style barGraph 
-        // barGraph.style.height = '90%'
-        // barGraph.style.backgroundColor = 'blue'
